a971f428ce2812f78a0beb3ae0b69010aaa4cd9f
dce23-git <dhirenchugh43@gmail.com>
2021-12-29 15:26:32 +0530

    chat app pushed

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const { read } = require("fs");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const app = express();
+const server = require("http").createServer(app);
+const io = require('socket.io')(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.engine("ejs", ejsMate);
+app.set("views", path.join(__dirname, "views"));
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+
+const data = [
+    {
+        user: "dhiren",
+        password: "password"
+    },
+    {
+        user: "ram",
+        password: "password"
+    },
+    {
+        user: "paul",
+        password: "password"
+    },
+]
+
+let auth = false; // login status
+
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+
+app.post("/login", (req, res) => {
+
+    const { user, password } = req.body;
+    console.log(req.body);
+
+    if (!user || !password) {
+        res.json({ status: "username or password not provided" });
+    }
+    else {
+        if (data.some(cred => (cred.user == user && cred.password === password)) ) {
+            // res.json({ status: "login succesfull" });
+            auth = true;
+            res.redirect("chat");
+        }
+        else {
+            res.json({ status: "invalid credentials" });
+        }
+    }
+});
+
+
+app.get("/chat", (req, res) => {
+    console.log(auth);
+    if (!auth) {
+        res.redirect("/login");
+    }
+    else res.render("chat");
+})
+
+
+// socket logic
+
+io.on("connect", (socket) => {
+
+    const socketId = socket.id;
+
+    socket.emit("joined", socketId);
+
+    socket.on("sendMessage", (data) => {
+        console.log("message received");
+        io.emit("receiveMessage", data, socketId);
+    })
+
+    socket.on("disconnect", (socket) => {
+        console.log(`Socket with Id: ${socketId} disconnected`);
+    })
+
+})
+
+
+
+
+server.listen(3000, () => {
+
+    console.log("listening");
+})
+
+
